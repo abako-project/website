8fdd72c331e03fe8d547a232cf6ac11744910ba6
Santiago Pavón <santiago.pavon@upm.es>
2025-11-27 15:17:43 +0100

    Publicar CSS y otros

--- backend/public/javascripts/utils.js
+++ backend/public/javascripts/utils.js
@@ -2,12 +2,37 @@
 /*
  * Ejecutar una promesa con un timeout.
  * Ejemplo:
- *    const r = await withTimeout(fetch(url), 5000)
+ *    try {
+ *        const r = await promiseWithTimeout(fetch(url), 5000);
+ *    } catch(err) {
+ *        console.log(err.message);
+ *    }
  */
-function withTimeout(promise, ms, msg) {
+function promiseWithTimeout(promise, ms, msg) {
     const timeout = new Promise((_, reject) =>
         setTimeout(() => reject(new Error(msg ?? "Timeout exceeded")), ms)
     );
 
     return Promise.race([promise, timeout]);
+}
+
+/*
+ * Ejecutar un fetch con un timeout.
+ * Ejemplo:
+ *    try {
+ *        const r = await fetchWithTimeout(url, 5000);
+ *    } catch(err) {
+ *        console.log(err.message);
+ *    }
+ */
+async function fetchWithTimeout(url, ms) {
+    const controller = new AbortController();
+    const id = setTimeout(() => controller.abort(), ms);
+
+    try {
+        const res = await fetch(url, { signal: controller.signal });
+        return res;
+    } finally {
+        clearTimeout(id);
+    }
 }
